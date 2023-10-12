75c8484016404c02ea2215c26b2e950b7a2058e2
Michał Łowkiet <m.lowkiet@openmobi.pl>
2023-10-12 13:28:49 +0200

    Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,45 @@
-export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
-}
+import Nav from './components/nav/Nav';
+import {
+  Hero,
+  PopularProducts,
+  Subscribe,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+  Footer,
+} from './sections';
+
+const App = () => {
+  return (
+    <main className='relative'>
+      <Nav />
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffers />
+      </section>
+      <section className='padding bg-pale-blue'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className='padding-x padding-t bg-black pb-8'>
+        <Footer />
+      </section>
+    </main>
+  );
+};
+
+export default App;
